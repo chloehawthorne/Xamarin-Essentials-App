dabac7ad8b95cf072a3738f7e773acd9421e571f
Chloe Hawthorne <cmhawthorne02@gmail.com>
2020-04-28 13:48:15 +0100

    latest changes (doesn't work)

---
+++ XamarinEssentials/XamarinEssentials/Model/ButtonModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XamarinEssentials.Model
+{
+    public class ButtonModel
+    {
+        public string Name { get; set; }
+        public string Command { get; set; }
+    }
+}
---
+++ XamarinEssentials/XamarinEssentials/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Text;
+using XamarinEssentials.Model;
+
+namespace XamarinEssentials.ViewModels
+{
+    public class MainPageViewModel :INotifyPropertyChanged
+    {
+        //ObservableCollection<ButtonModel> measurement;
+
+        public ObservableCollection<ButtonModel> Measurement { get; set; } = new ObservableCollection<ButtonModel>();
+
+        ButtonModel selectedButton;
+
+        public ButtonModel SelectedButton
+        {
+            get
+            {
+                return selectedButton;
+            }
+            set
+            {
+                if (selectedButton !=value)
+                {
+                    selectedButton = value;
+                }
+            }
+        }
+          
+        
+    public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
